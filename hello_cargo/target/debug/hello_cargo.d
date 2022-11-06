f0bd039400e8eae8c7ef95776a8a85d635efa266
YoungJ-Baek <qordudwls1@gmail.com>
2022-11-06 22:57:08 +0900

    Print hello world with rustc and cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/youngjin/Desktop/projects/hello_cargo/target/debug/hello_cargo: /Users/youngjin/Desktop/projects/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/deps/hello_cargo-69a02a585e67d3c0.d
@@ -0,0 +1,5 @@
+/Users/youngjin/Desktop/projects/hello_cargo/target/release/deps/hello_cargo-69a02a585e67d3c0: src/main.rs
+
+/Users/youngjin/Desktop/projects/hello_cargo/target/release/deps/hello_cargo-69a02a585e67d3c0.d: src/main.rs
+
+src/main.rs:
