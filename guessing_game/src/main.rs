247395d25f77ccaec4a376fedfd00d7b2f2111ea
YoungJ-Baek <qordudwls1@gmail.com>
2022-11-19 00:26:14 +0900

    Add match expressions

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,23 +5,32 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100).to_string();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        // let apple = 5 // immutable
+        let mut guess = String::new(); // mutable
 
-    let mut guess = String::new(); // mutable
-                                   // let apple = 5 // immutable
-    io::stdin()
-        .read_line(&mut guess) // it is important to add &mut to modify references; if you use &guess, it is immutable
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess) // it is important to add &mut to modify references; if you use &guess, it is immutable
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
